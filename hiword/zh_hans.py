3a115536599ecdc2d9ddc1490d5b2c484e958c2f
jadbin <jadbin.com@hotmail.com>
2021-02-08 10:40:08 +0800

    简化代码

--- hiword/zh_hans.py
+++ hiword/zh_hans.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 
-from os.path import join, dirname
 from multiprocessing import Lock
+from os.path import join, dirname
 
 zh_hans = {}
 zh_hans_lock = Lock()
 
 
-def traditional_to_simple(text):
+def traditional_to_simple(text: str):
     with zh_hans_lock:
         if len(zh_hans) == 0:
             file = join(dirname(__file__), 'data', 'zh_hans.txt')
@@ -17,12 +17,6 @@ def traditional_to_simple(text):
                     if not line:
                         break
                     s = line.split()
-                    zh_hans[s[0]] = s[1]
+                    zh_hans[ord(s[0])] = ord(s[1])
 
-    res = []
-    for i in text:
-        if i in zh_hans:
-            res.append(zh_hans[i])
-        else:
-            res.append(i)
-    return ''.join(res)
+    return text.translate(zh_hans)
