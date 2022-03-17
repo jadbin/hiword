a89d4827d2c140ad01ca6433e51ec923d41ee63b
jadbin <jadbin.com@hotmail.com>
2022-03-17 18:45:45 +0800

    bump to v0.5.2

--- hiword/__init__.py
+++ hiword/__init__.py
@@ -1,7 +1,7 @@
 from hiword.extractor import KeywordsExtractor
 from hiword.extractor import extract_keywords
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 __all__ = [
     'KeywordsExtractor',
